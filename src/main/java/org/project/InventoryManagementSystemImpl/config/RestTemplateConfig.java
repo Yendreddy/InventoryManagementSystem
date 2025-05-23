cb3bf944e567caa41601ea4b46dbb71a9fde4437
Yendreddy <149788697+Yendreddy@users.noreply.github.com>
2025-05-23 19:30:56 +0530

    Update RestTemplateConfig.java

--- src/main/java/org/project/InventoryManagementSystemImpl/config/RestTemplateConfig.java
+++ src/main/java/org/project/InventoryManagementSystemImpl/config/RestTemplateConfig.java
@@ -1,45 +1,45 @@
-package org.project.InventoryManagementSystemImpl.config;
+// package org.project.InventoryManagementSystemImpl.config;
 
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
-import org.apache.hc.client5.http.ssl.TrustAllStrategy;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
-import org.apache.hc.core5.util.Timeout;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+// import org.apache.hc.client5.http.config.RequestConfig;
+// import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+// import org.apache.hc.client5.http.impl.classic.HttpClients;
+// import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+// import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+// import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+// import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+// import org.apache.hc.core5.ssl.SSLContextBuilder;
+// import org.apache.hc.core5.util.Timeout;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+// import org.springframework.web.client.RestTemplate;
 
-@Configuration
-public class RestTemplateConfig {
-    @Bean
-    public RestTemplate restTemplate() throws Exception {
-        SSLContextBuilder sslContextBuilder = SSLContextBuilder.create()
-                .loadTrustMaterial(new TrustAllStrategy());
+// @Configuration
+// public class RestTemplateConfig {
+//     @Bean
+//     public RestTemplate restTemplate() throws Exception {
+//         SSLContextBuilder sslContextBuilder = SSLContextBuilder.create()
+//                 .loadTrustMaterial(new TrustAllStrategy());
 
-        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
-                        .setSslContext(sslContextBuilder.build())
-                        .build())
-                .setValidateAfterInactivity(Timeout.ofSeconds(1)) // Set validateAfterInactivity
-                .build();
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectionKeepAlive(Timeout.ofSeconds(5))
-                        .setResponseTimeout(Timeout.ofSeconds(5))
-                        .setConnectTimeout(Timeout.ofSeconds(5))
-                        .build())
-                .setConnectionReuseStrategy((httpRequest, httpResponse, httpContext ) -> false) // Disable connection reuse
-                .build();
+//         PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+//                 .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
+//                         .setSslContext(sslContextBuilder.build())
+//                         .build())
+//                 .setValidateAfterInactivity(Timeout.ofSeconds(1)) // Set validateAfterInactivity
+//                 .build();
+//         CloseableHttpClient httpClient = HttpClients.custom()
+//                 .setConnectionManager(connectionManager)
+//                 .setDefaultRequestConfig(RequestConfig.custom()
+//                         .setConnectionKeepAlive(Timeout.ofSeconds(5))
+//                         .setResponseTimeout(Timeout.ofSeconds(5))
+//                         .setConnectTimeout(Timeout.ofSeconds(5))
+//                         .build())
+//                 .setConnectionReuseStrategy((httpRequest, httpResponse, httpContext ) -> false) // Disable connection reuse
+//                 .build();
 
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectTimeout(5000); // 5 seconds
-        factory.setReadTimeout(5000); // 5 seconds
-        return new RestTemplate(factory);
-    }
-}
+//         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//         factory.setConnectTimeout(5000); // 5 seconds
+//         factory.setReadTimeout(5000); // 5 seconds
+//         return new RestTemplate(factory);
+//     }
+// }
